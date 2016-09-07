3f75bcf67a3895c3c25eaa30f7efb86325caa31d
dispiste <cesar.izq@gmail.com>
2016-09-07 10:05:23 +0200

    Update license to GPL2+, as GPL code is included in the library

--- console.py
+++ console.py
@@ -3,6 +3,21 @@ Jython Console with Code Completion
 
 This uses the basic Jython Interactive Interpreter.
 The UI uses code from Carlos Quiroz's 'Jython Interpreter for JEdit' http://www.jedit.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
 from javax.swing import JFrame, JScrollPane, JWindow, JTextPane, Action, KeyStroke, WindowConstants
 from javax.swing.text import JTextComponent, TextAction, SimpleAttributeSet, StyleConstants
